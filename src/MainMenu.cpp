7a8ef1b6bcb0cd2bd005b0ab434a89d638defa1e
Tim Xu <timxuti@gmail.com>
2024-12-03 21:05:49 -0800

    MainMenu Class Initiated

---
+++ src/MainMenu.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Tim on 12/3/2024.
+//
+
+#include "MainMenu.h"
---
+++ src/MainMenu.h
@@ -0,0 +1,16 @@
+//
+// Created by Tim on 12/3/2024.
+//
+
+#ifndef MAINMENU_H
+#define MAINMENU_H
+
+
+
+class MainMenu {
+
+};
+
+
+
+#endif //MAINMENU_H
