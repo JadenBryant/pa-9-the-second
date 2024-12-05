6224f8019fe6d73db30fec9d131efcf0d8798b2b
Daniel Bereza <blackcatmaxy@gmail.com>
2024-12-05 10:43:03 -0800

    Add jumping and change player.png to fully show player bounds

--- src/Player.cpp
+++ src/Player.cpp
@@ -5,32 +5,46 @@ Player::Player(std::vector<Block> *blocks) {
     if (!texture.loadFromFile("resources/player.png")) {
         std::cerr << "Failed to load texture" << std::endl;
     }
-    this->setTexture(texture);
-    this->setPosition(200, 200);
-
+    setTexture(texture);
+    setPosition(200, 200);
+    setOrigin(texture.getSize().x / 2, texture.getSize().y / 2);
     this->blocks = blocks;
+
     // CAMERA CODE
     this->Camera.setCenter(this->getPosition());
 }
 
 void Player::updatePhysics(sf::Time time) {
-    float speed = 75.0f;
+    auto position = getPosition();
+    float moveSpeed = 150.0f, jumpSpeed = 250.0f;
+    float dt = time.asSeconds();
+    float dg = 100 * dt;
+
+    //side movement
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
     {
-        velocity.x = -speed;
+        velocity.x = -moveSpeed;
     }
     else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
     {
-        velocity.x = speed;
+        velocity.x = moveSpeed;
     } else {
         velocity.x = 0;
     }
 
-    float dt = time.asSeconds();
-    float dy = 50 * dt;
+    velocity.y += dg;
+    if (velocity.y > 0)
+        velocity.y = 0;
+
+    //jump code
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+
+    }
+
     float dx = velocity.x * dt;
-    auto position = getPosition();
-    std::cout << "Player physics: " << position.y << " " << dt << " " << position.y + dy << std::endl;
+    float dy = velocity.y * dt;
+    std::cout << "Player physics: " << position.y << " " << dt << " " << position.y + dg << std::endl;
+    dy += dg;
     this->setPosition(position.x + dx, position.y + dy);
 
     auto playerBounds = getGlobalBounds();
@@ -38,9 +52,7 @@ void Player::updatePhysics(sf::Time time) {
         auto blockBounds = block.getGlobalBounds();
         // std::cout << "Player test" << blockBounds.width << " " << blockBounds.height << " " << blockBounds.getPosition().x << " "<<  blockBounds.getPosition().y << std::endl;
         if (block.getGlobalBounds().intersects(playerBounds)) {
-            dx = 0;
-            dy = -dy;
-            this->setPosition(position.x + dx, position.y + dy);
+            this->setPosition(position.x, position.y - dy);
             break;
             // std::cout << "Player collision: " << position.y << " " << dt << " " << position.y + dy << std::endl;
         }
