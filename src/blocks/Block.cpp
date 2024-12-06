7b080bf1d51373bad9de913d079f5c0488f1f178
Jaden Bryant <hello@jadenbryant.dev>
2024-12-06 08:50:30 -0800

    Added comments to Block.cpp

--- src/blocks/Block.cpp
+++ src/blocks/Block.cpp
@@ -5,14 +5,14 @@
 #include "Block.h"
 #include <random>
 
-std::random_device rd;                               // Seed
-std::mt19937 gen(rd());                              // Mersenne Twister generator
-std::uniform_int_distribution<> dis(0, 255);         // Range for RGB values
+std::random_device rd;                               
+std::mt19937 gen(rd());
+std::uniform_int_distribution<> dis(0, 255);
 
 
 Block::Block(sf::Vector2f position) {
     this->setSize(sf::Vector2f(50.f, 50.f));
-    this->setFillColor(sf::Color(dis(gen), dis(gen), dis(gen)));
+    this->setFillColor(sf::Color(dis(gen), dis(gen), dis(gen))); # randomly generating block colors
     this->setPosition(position);
 
     this->Type = "Block";
@@ -20,7 +20,7 @@ Block::Block(sf::Vector2f position) {
 
 Block::Block(int x, int y) {
     setSize(sf::Vector2f(50.f, 50.f));
-    this->setFillColor(sf::Color(dis(gen), dis(gen), dis(gen)));
+    this->setFillColor(sf::Color(dis(gen), dis(gen), dis(gen))); # randomly generating block colors
     setPosition(x, y);
 
     this->Type = "Block";
