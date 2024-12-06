7151b7e3ce98dfaac70b147d115e257ea757c88a
Tim Xu <timxuti@gmail.com>
2024-12-05 21:58:29 -0800

    Game class complete, wiped main

--- src/Game.cpp
+++ src/Game.cpp
@@ -3,3 +3,95 @@
 //
 
 #include "Game.h"
+
+Game::Game()
+    : window(sf::VideoMode(1920, 1080), "Racing Game"),
+      isGameOver(false),
+      timeToComplete(60),
+      timeRemaining(999),
+      gameMap(window.getSize()),
+      blocks(gameMap.blocks),
+      player(&blocks),
+      playerCamera(player.Camera) {
+    window.setFramerateLimit(144);
+    playerCamera.setSize(sf::Vector2f(window.getSize()));
+
+    // Load font
+    font.loadFromFile("resources/WorkSans-Regular.ttf");
+
+    // Initialize GUI elements
+    clockGui.setFont(font);
+
+    gameOverModal.setOutlineThickness(3);
+    gameOverModal.setOutlineColor(sf::Color::Black);
+    gameOverModal.setSize(sf::Vector2f(window.getSize().x / 3, window.getSize().y / 3));
+    gameOverModal.setOrigin(gameOverModal.getSize().x / 2, gameOverModal.getSize().y / 2);
+    gameOverModal.setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
+
+    gameOverText.setFont(font);
+    gameOverText.setFillColor(sf::Color::Black);
+    gameOverText.setString("Game over!");
+    gameOverText.setOrigin(gameOverText.getLocalBounds().width / 2, gameOverText.getLocalBounds().height / 2);
+    gameOverText.setPosition(sf::Vector2f(window.getSize().x / 2, window.getSize().y / 2));
+}
+
+void Game::run() {
+    while (window.isOpen()) {
+        processEvents();
+        update();
+        render();
+    }
+}
+
+void Game::processEvents() {
+    for (auto event = sf::Event(); window.pollEvent(event);) {
+        if (event.type == sf::Event::Closed) {
+            window.close();
+        }
+    }
+}
+
+void Game::update() {
+    // ATTRIBUTES
+    int timeRunningInSeconds = static_cast<int>(gameClock.getElapsedTime().asSeconds());
+    if (timeRemaining > 0) {
+        timeRemaining = timeToComplete - timeRunningInSeconds;
+    } else {
+        isGameOver = true;
+    }
+
+    // PLAYER MOVEMENT
+    sf::Time elapsed = clock.restart();
+    player.updatePhysics(elapsed);
+
+    // PLAYER CAMERA
+    playerCamera.setCenter(sf::Vector2f(player.getPosition().x, player.getPosition().y));
+}
+
+void Game::render() {
+    window.clear(sf::Color(105, 203, 224));
+
+    // DRAW WORLD
+    window.setView(playerCamera);
+    for (const auto& block : blocks) {
+        window.draw(block);
+    }
+    window.draw(player);
+
+    // GUI CODE
+    auto bounds = clockGui.getLocalBounds();
+    clockGui.setOrigin(bounds.width / 2, bounds.top);
+    clockGui.setPosition(window.getSize().x / 2, 5);
+    clockGui.setString("Time Remaining: " + std::to_string(timeRemaining));
+
+    // DRAW GUI
+    window.setView(window.getDefaultView());
+    window.draw(clockGui);
+
+    if (isGameOver) {
+        window.draw(gameOverModal);
+        window.draw(gameOverText);
+    }
+
+    window.display();
+}
--- src/Game.h
+++ src/Game.h
@@ -5,12 +5,36 @@
 #ifndef GAME_H
 #define GAME_H
 
-
+#include <SFML/Graphics.hpp>
+#include "Player.h"
+#include "Map.h"
+#include "blocks/Block.h"
+#include "blocks/FinishBlock.h"
 
 class Game {
+public:
+    Game();
+    void run();
 
+private:
+    void processEvents();
+    void update();
+    void render();
+
+    sf::RenderWindow window;
+    bool isGameOver;
+    int timeToComplete;
+    int timeRemaining;
+    sf::Clock gameClock;
+    sf::Clock clock;
+    Map gameMap;
+    std::vector<Block> blocks;
+    Player player;
+    sf::View playerCamera;
+    sf::Font font;
+    sf::Text clockGui;
+    sf::RectangleShape gameOverModal;
+    sf::Text gameOverText;
 };
 
-
-
-#endif //GAME_H
+#endif // GAME_H
