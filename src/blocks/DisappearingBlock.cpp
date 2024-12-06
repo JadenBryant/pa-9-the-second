a3e5189f21e140e0fb0bf144ba51175627c017cc
Jackson Lusk <jlusk130@gmail.com>
2024-12-06 00:38:41 -0800

    Continuing disappearing block

--- src/blocks/DisappearingBlock.cpp
+++ src/blocks/DisappearingBlock.cpp
@@ -4,18 +4,24 @@
 
 #include "DisappearingBlock.h"
 
+#include <iostream>
+#include <ostream>
 
+//Constructor
 DisappearingBlock::DisappearingBlock(sf::Vector2f position) : Block(position), transparency(255),
-isCollidable(true) {
+                                                              isCollidable(true) {
     isCollidable = true;
     this->setFillColor(sf::Color(0, 255, 0, 255));
     this->Type = "DisappearingBlock";
 }
+//Copy constructor
 DisappearingBlock::DisappearingBlock(int x, int y):
 DisappearingBlock(sf::Vector2f(static_cast<float>(x), static_cast<float>(y))) {
 
 
 }
+
+//This function will test to see if the player interacted with the block
 void DisappearingBlock::onTouch(Player &whoTouched) {
 
 if (isCollidable) {
@@ -26,5 +32,5 @@ if (isCollidable) {
     sf::Color currColor = this->getFillColor();
     this->setFillColor(sf::Color(currColor.r, currColor.g, currColor.b, static_cast<sf::Uint8>(transparency)));
     }
-
+    std::cout << "DisappearingBlock::onTouch" << std::endl;
 }
